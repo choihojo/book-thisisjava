959a1d2498678bf0f530ff84778f1765077f662d
Choi Hojo <choihojo@outlook.com>
2024-06-11 20:19:21 +0900

    doc: 13.1 제네릭이란? 업데이트

--- src/ch13/sec01/Box.java
+++ src/ch13/sec01/Box.java
@@ -2,4 +2,12 @@ package ch13.sec01;
 
 public class Box<T> {
 
+    public T testMethod1(T param) {
+        return param;
+    }
+
+    public <T> T testMethod2(T param) {
+        return param;
+    }
+
 }
--- src/ch13/sec01/Practice.java
+++ src/ch13/sec01/Practice.java
@@ -1,10 +1,18 @@
 package ch13.sec01;
 
+import java.util.ArrayList;
+
 public class Practice {
 
     public static void main(String[] args) {
         Box<String> box = new Box<String>();
-        System.out.println(1111);
+//        Box<String>[] boxArr = new Box<>[10]; // Array creation with '<>' not allowed
+        Box<String>[] boxArr = new Box[10]; // Unchecked assignment: 'ch13.sec01.Box[]' to 'ch13.sec01.Box<java.lang.String>[]'
+        ArrayList<Box<String>> boxList = new ArrayList<Box<String>>();
+
+        String test1 = box.testMethod1("테스트1");
+        int test2 = box.testMethod2(2);
+        Integer test3 = box.<Integer>testMethod2(3);
     }
 
 }
