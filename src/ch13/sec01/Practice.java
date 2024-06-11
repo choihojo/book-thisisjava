714cbd525558813c6804f7f3c1c8c46e7d764a20
Choi Hojo <choihojo@outlook.com>
2024-06-11 15:25:30 +0900

    doc: 13.1 제네릭이란? 업데이트

---
+++ src/ch13/sec01/Practice.java
@@ -0,0 +1,10 @@
+package ch13.sec01;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        Box<String> box = new Box<String>();
+        System.out.println(1111);
+    }
+
+}
