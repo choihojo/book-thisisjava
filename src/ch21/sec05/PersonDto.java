1b780c7831d84ab38d2afc71de242a09f5a22cfd
Choi Hojo <choihojo@outlook.com>
2024-06-22 16:16:07 +0900

    doc: 21.5 레코드 패턴 업데이트

---
+++ src/ch21/sec05/PersonDto.java
@@ -0,0 +1,9 @@
+package ch21.sec05;
+
+public class PersonDto {
+
+    public record Request(String id, String name) {
+
+    }
+
+}
---
+++ src/ch21/sec05/RecordExample.java
@@ -0,0 +1,9 @@
+package ch21.sec05;
+
+public class RecordExample {
+
+    public static void main(String[] args) {
+        PersonDto.Request request = new PersonDto.Request("1", "홍길동");
+    }
+
+}
